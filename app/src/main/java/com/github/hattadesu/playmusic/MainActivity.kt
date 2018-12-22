84171e1357f8b0428064906758b7decd77e117e6
eight8ta <hatta.desu@gmail.com>
2018-12-22 07:07:05 +0000

    Added play command

--- app/src/main/java/com/github/hattadesu/playmusic/MainActivity.kt
+++ app/src/main/java/com/github/hattadesu/playmusic/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import android.widget.Button
+import android.content.Intent
+import android.provider.MediaStore
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<View>(R.id.button_play) as Button
         button.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Tapped", Toast.LENGTH_LONG).show()
+            //Toast.makeText(this@MainActivity, "Tapped", Toast.LENGTH_LONG).show()
+            val intent = Intent("com.android.music.musicservicecommand")
+            intent.putExtra("command", "play")
+            sendBroadcast(intent)
         }
     }
 }
