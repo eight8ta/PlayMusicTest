a8ef3d0191b85d249c0cdacff6478323315e33b1
eight8ta <hatta.desu@gmail.com>
2018-12-22 08:48:13 +0000

    Removed empty lines

--- app/src/main/java/com/github/hattadesu/playmusic/MainActivity.kt
+++ app/src/main/java/com/github/hattadesu/playmusic/MainActivity.kt
@@ -6,9 +6,7 @@ import android.view.View
 import android.widget.Toast
 import android.widget.Button
 import android.content.Intent
-import android.provider.MediaStore
-
-
+//import android.provider.MediaStore
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<View>(R.id.button_play) as Button
         button.setOnClickListener {
             //Toast.makeText(this@MainActivity, "Tapped", Toast.LENGTH_LONG).show()
+/*
+            val intent = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_MUSIC);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);//Min SDK 15
+            val intent = Intent(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH)
+            intent.putExtra(MediaStore.EXTRA_MEDIA_FOCUS, "vnd.android.cursor.item/audio")
+            startActivity(intent);
+*/
             val intent = Intent("com.android.music.musicservicecommand")
             intent.putExtra("command", "play")
             sendBroadcast(intent)
